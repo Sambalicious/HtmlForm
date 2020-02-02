396268111b6bbd078c9c09decaebda7f254461e6
samuel Temitope <stemitope370@gmail.com>
2020-02-02 21:15:04 +0100

    validations

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', signUp);
+
+
+function signUp(e){
+
+const name = document.getElementById('fullname').value;
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+const cpass = document.getElementById('cpass').value;
+//const checkbox = document.querySelector('input[type ="checkbox"]:checked').value;
+
+if (name.trim() && username.trim() && password && cpass){
+    e.preventDefault();
+
+    if (password === cpass){
+        console.log(`your name is ${name}`);
+        console.log(`your username is ${username}`);
+        console.log(`your password is ${password}`);
+        console.log(`you confirm password is ${cpass}`);
+        
+        alert(`hello ${username}, You are now logged in`);
+    }
+    else{
+    if (name == '' || username === ''){
+        alert(`Enter a valid input`)
+    }
+    }
+}
+
+}
